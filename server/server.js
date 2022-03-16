e02098fc43cdd5f8e462066d4bf140c3f65b1ea0
Maciej Sierżęga <61496946+maciekm98@users.noreply.github.com>
2022-03-16 19:39:56 +0100

    sequelize

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-
+const db = require("./models");
+const { student } = require("./models");
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-
+db.sequelize.sync()
 
 
 app.listen (5000, () => {
