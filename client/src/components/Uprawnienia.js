d10ff1697b6c397a56454f9b43cbce3a988349a0
Maciej Sierżęga <61496946+maciekm98@users.noreply.github.com>
2022-05-25 22:02:06 +0200

    uprawnienia

--- client/src/components/Uprawnienia.js
+++ client/src/components/Uprawnienia.js
@@ -1,7 +1,62 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 export default function Uprawnienia() {
+
+
+ const $ = require('jquery')
+ $.DataTable = require('datatables.net')
+
+  $(document).ready(function () {
+      $('#accounts').DataTable();
+  });
+
+
+
+
+  const [students,setStudents]=useState([])
+  useEffect(()=>
+  {
+
+    axios.get("http://localhost:5000/api/getStudents").then((res)=>{
+      
+    setStudents(res.data)
+      
+    })
+  },[])
+
+
   return (
-    <div></div>
+ <div>
+    <table id="accounts" class="display">
+    <thead>
+        <tr>
+            <th>Login</th>
+            <th>Student</th>
+            <th>Opiekun Zakładowy</th>
+            <th>Opiekun Uczelniany</th>
+            <th>Dziekanat</th>
+            <th>Dyrektor</th>
+            <th>Admin</th>
+        </tr>
+    </thead>
+    <tbody>
+      
+    {students.map((val)=> {
+      return (
+
+        <tr>
+            <td>{val.login}</td>
+            <td>{val.isStudent}</td>
+            <td>{val.isOpiekunZakl}</td>
+            <td>{val.isOpiekun}</td>
+            <td>{val.isDziekanat}</td>
+            <td>{val.isDyrektor}</td>
+            <td>{val.isAdmin}</td>
+        </tr>
+      )
+    })}
+        </tbody>
+        </table>
+        </div>
   )
 }
