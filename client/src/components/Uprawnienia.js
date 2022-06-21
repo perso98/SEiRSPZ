67074b9f3ca35064e7bab2db6951b5b1560ddeb3
Maciej Sierżęga <61496946+maciekm98@users.noreply.github.com>
2022-06-21 19:50:16 +0200

    tabela działa

--- client/src/components/Uprawnienia.js
+++ client/src/components/Uprawnienia.js
@@ -1,162 +1,203 @@
-import axios from 'axios'
-import {makeStyles, Table, TableBody, TableCell, TableHead, TablePagination, TableRow, Toolbar, TextField, Button} from '@material-ui/core'
-import React, { useState, useEffect } from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import { InputAdornment } from '@mui/material';
-const useStyles = makeStyles(theme=>({
-  table:{
-    marginTop:theme.spacing(2),
-    width:'70%',
-    '& thead th':{
-      fontWeight: '600',
-      color:'white',
+import axios, * as others from "axios";
+import {
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Toolbar,
+  TextField,
+} from "@material-ui/core";
+import Button from "@mui/material/Button";
+import React, { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { InputAdornment } from "@mui/material";
+const useStyles = makeStyles((theme) => ({
+  table: {
+    marginTop: theme.spacing(2),
+    width: "70%",
+    "& thead th": {
+      fontWeight: "600",
+      color: "white",
     },
-  '& tbody tr:hover':{
-    backgroundColor:'gray',
+    // '& tbody tr:hover':{
+    //   backgroundColor:'gray',
+    // },
   },
-
-
-},
-tableHead:{
-  background:'#08448c',
-  
-},
-searchInp:{
-  width: '40%',
-},
-toolbar:{
-  marginTop:'2%',
-},
-NoData:{
-  display:'flex',
-  justifyContent:'space-between',
-  fontWeight:'600',
-}
-}))
+  tableHead: {
+    background: "#08448c",
+  },
+  searchInp: {
+    width: "40%",
+  },
+  toolbar: {
+    marginTop: "2%",
+  },
+  NoData: {
+    display: "flex",
+    justifyContent: "space-between",
+    fontWeight: "600",
+  },
+}));
 export default function Uprawnienia() {
+  const classes = useStyles();
 
-  const classes = useStyles()
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/getStudents").then((res) => {
+      setStudents(res.data);
+      setLoading(false);
+    });
+  }, []);
 
-  const [students,setStudents]=useState([])
-  const [loading,setLoading]=useState(true)
-  useEffect(()=>
-  {
+  const pages = [10, 15, 20];
+  const [page, setPage] = useState(0);
+  const [pageRows, setpageRows] = useState(pages[page]);
+  const [searchLogin, setSearchLogin] = useState("");
 
-    axios.get("http://localhost:5000/api/getStudents").then((res)=>{
-      
-    setStudents(res.data)
-    setLoading(false)
-      
-    })
-  },[])
-
-  const pages = [10,15,20]
-  const [page,setPage]=useState(0)
-  const [pageRows,setpageRows]=useState(pages[page])
-  const [searchLogin,setSearchLogin]=useState('')
-
- 
   const HeadCells = [
-    {id: 'login',label:'Login'},
-    {id: 'isStudent',label:'Student'},
-    {id: 'isAdmin',label:'Admin'},
-    {id: 'isOpiekunZakl',label:'Opiekun Zakł.'},
-    {id: 'isOpiekun',label:'Opiekun Ucz.'},
-    {id: 'isDyrektor',label:'Dyrektor'},
-    {id: 'isDziekanat',label:'Dziekanat'},
-  ]
-  
-  const handleChangePage = (event,newPage)=>{
-    setPage(newPage)
+    { id: "login", label: "Login" },
+    { id: "isStudent", label: "Student" },
+    { id: "isAdmin", label: "Admin" },
+    { id: "isOpiekunZakl", label: "Opiekun Zakł." },
+    { id: "isOpiekun", label: "Opiekun Ucz." },
+    { id: "isDyrektor", label: "Dyrektor" },
+    { id: "isDziekanat", label: "Dziekanat" },
+  ];
 
-  }
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-  const handleChangeRowsPerPage = (event)=>{
-    setpageRows(parseInt(event.target.value,10))
-    setPage(0)
-  }
-  const recordsAfterFiltering=students.filter((val)=>{
-      if(searchLogin==''){
-        return val}
-
-      else if (val.login.toLowerCase().includes(searchLogin.toLowerCase())){
-      return val }           
-    })
-    const recordsAfter = ()=>{
-      return recordsAfterFiltering.slice(page*pageRows,(page+1)*pageRows)
+  const handleChangeRowsPerPage = (event) => {
+    setpageRows(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const recordsAfterFiltering = students.filter((val) => {
+    if (searchLogin == "") {
+      return val;
+    } else if (val.login.toLowerCase().includes(searchLogin.toLowerCase())) {
+      return val;
     }
-   
-  
+  });
+  const recordsAfter = () => {
+    return recordsAfterFiltering.slice(page * pageRows, (page + 1) * pageRows);
+  };
 
+  const updateRole = (action, type, id) => {
+    axios
+      .put("http://localhost:5000/api/changeRole", {
+        action: action,
+        type: type,
+        id: id,
+      }).then((res)=>{
+        setStudents(
+        students.map((val) => {
+          return val.id == id ? { ...val, [action]: type } : val;
+        })
+      )});
+  };
+  
+  const giveButton = (action,id) => { 
+    return ( <Button
+      style={{width:'70px'}}
+      variant="contained"
+      color="success"
+      onClick={() => {
+        updateRole(action, 1, id);
+      }}
+    >
+      Nadaj
+    </Button>)
+  }
+  const takeButton = (action,id) =>
+  {
+    return ( <Button
+      style={{width:'70px'}}
+      variant="contained"
+      color="error"
+      onClick={() => {
+        updateRole(action, 0, id);
+      }}
+    >
+      Odbierz
+    </Button>)
+  }
 
   return (
-    
-    
-<>
-    <Toolbar className={classes.toolbar}>
-    <TextField
-    className={classes.searchInp}
-      label="Szukaj"
-      variant='outlined'
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-      
-      onChange={(e)=>{
-        setSearchLogin(e.target.value)
-      }}
-    />
-    </Toolbar>
+    <>
+      <Toolbar className={classes.toolbar}>
+        <TextField
+          className={classes.searchInp}
+          label="Szukaj"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(e) => {
+            setSearchLogin(e.target.value);
+          }}
+        />
+      </Toolbar>
 
-<Table className={classes.table}>
-<TableHead className={classes.tableHead} >
-    <TableRow >
-      {
-        HeadCells.map(head => (<TableCell key={head.id}>
-          {head.label}
-        </TableCell>))
-      }
-    </TableRow>
-    </TableHead >
-  <TableBody>
-    
-  {loading==true &&<TableRow>Ładowanie...</TableRow>}
-  {(recordsAfterFiltering.length==0 && loading==false) && <TableRow className={classes.NoData}>Brak danych...</TableRow>}
-    {
-     recordsAfter().map(val => 
-        (
-          <TableRow key={val.id}>
-            <TableCell>{val.login}</TableCell>
-            <TableCell >{val.isStudent==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button> }</TableCell>
-            <TableCell>{val.isAdmin==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button>  }</TableCell>
-            <TableCell>{val.isOpiekunZakl==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button>  }</TableCell>
-            <TableCell>{val.isOpiekun==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button>  }</TableCell>
-            <TableCell>{val.isDyrektor==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button>  }</TableCell>
-            <TableCell>{val.isDziekanat==1? <button style={{background:'red'}}>Odbierz</button>:<button style={{background:'green'}}>Nadaj</button>  }</TableCell>
-           
+      <Table className={classes.table}>
+        <TableHead className={classes.tableHead}>
+          <TableRow>
+            {HeadCells.map((head) => (
+              <TableCell style={{ textAlign: "center" }} key={head.id}>
+                {head.label}
+              </TableCell>
+            ))}
           </TableRow>
-        ))
-    }
-     
-
-    
-           
-  </TableBody>
-  <TablePagination
-  component="div"
-  page={page}
-  rowsPerPageOptions={pages}
-  rowsPerPage={pageRows}
-  count={recordsAfterFiltering.length}
-  onPageChange={handleChangePage}
-  onRowsPerPageChange={handleChangeRowsPerPage}  />
-</Table>
-</>
-
-
-  )
+        </TableHead>
+        <TableBody>
+          {loading == true && <TableRow>Ładowanie...</TableRow>}
+          {recordsAfterFiltering.length == 0 && loading == false && (
+            <TableRow className={classes.NoData}>Brak danych...</TableRow>
+          )}
+        
+          {recordsAfter().map((val) => (
+            <TableRow key={val.id}>
+              <TableCell>{val.login}</TableCell>
+              <TableCell>
+                {val.isStudent == 1 ? takeButton('isStudent',val.id): giveButton('isStudent',val.id)}
+              </TableCell>
+              <TableCell>
+                {val.isAdmin == 1 ? takeButton('isAdmin',val.id): giveButton('isAdmin',val.id)}
+              </TableCell>
+              <TableCell>
+                {val.isOpiekunZakl == 1 ? takeButton('isOpiekunZakl',val.id): giveButton('isOpiekunZakl',val.id)}
+              </TableCell>
+              <TableCell>
+                {val.isOpiekun == 1 ? takeButton('isOpiekun',val.id): giveButton('isOpiekun',val.id)}
+              </TableCell>
+              <TableCell>
+                {val.isDyrektor == 1 ? takeButton('isDyrektor',val.id): giveButton('isDyrektor',val.id)}
+              </TableCell>
+              <TableCell>
+                {val.isDziekanat == 1 ? takeButton('isDziekanat',val.id): giveButton('isDziekanat',val.id)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TablePagination
+          component="div"
+          page={page}
+          rowsPerPageOptions={pages}
+          rowsPerPage={pageRows}
+          count={recordsAfterFiltering.length}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Table>
+    </>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,196 +1,189 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
 const db = require("./models");
-const session = require('express-session')
-const cookieParser = require("cookie-parser")
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
-const {
-    student
-} = require("./models");
-const cors = require('cors')
-const bcrypt = require('bcrypt')
+const { student } = require("./models");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-app.use(express.json())
-app.use(cors(
-{
-    origin :["http://localhost:3000"],
-    methods : ["GET","POST"],
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
-}))
-app.use(cookieParser())
-app.use(session({
-    key:"user",
-    secret:"adsadsadkisahduja92183782uiwejdhjlkjsandkjbnsa",
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        expires:60*60*48000,
-    }
-}))
+  })
+);
+app.use(cookieParser());
+app.use(
+  session({
+    key: "user",
+    secret: "adsadsadkisahduja92183782uiwejdhjlkjsandkjbnsa",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 48000,
+    },
+  })
+);
 
-
-db.sequelize.sync()
-
+db.sequelize.sync();
 
 app.listen(5000, () => {
-    console.log('Serwer uruchomiony na porcie 5000')
-})
+  console.log("Serwer uruchomiony na porcie 5000");
+});
 
-app.post('/api/createAccount', async (req, res) => {
-    const {
-        registerLogin,
-        registerPassword,
-        registerPassword2
-    } = req.body
-    const loginChecker = await student.findOne({
-        where: {
-            login: registerLogin
-        }
-    })
-    if (loginChecker == null) {
-        if (registerPassword == registerPassword2) {
-            const hashedPassword = await bcrypt.hash(registerPassword, 10)
-            student.create({
-                login: registerLogin,
-                haslo: hashedPassword,
-            })
-            res.send({
-                message: 'Konto zostało pomyślnie utworzone'
-            });
-        } else
-            res.send({
-                message: 'Hasła się nie zgadzają'
-            });
-
+app.post("/api/createAccount", async (req, res) => {
+  const { registerLogin, registerPassword, registerPassword2 } = req.body;
+  const loginChecker = await student.findOne({
+    where: {
+      login: registerLogin,
+    },
+  });
+  if (loginChecker == null) {
+    if (registerPassword == registerPassword2) {
+      const hashedPassword = await bcrypt.hash(registerPassword, 10);
+      student.create({
+        login: registerLogin,
+        haslo: hashedPassword,
+      });
+      res.send({
+        message: "Konto zostało pomyślnie utworzone",
+      });
     } else
-        res.send({
-            message: 'Niestety taki login jest już zajęty'
-        });
-})
+      res.send({
+        message: "Hasła się nie zgadzają",
+      });
+  } else
+    res.send({
+      message: "Niestety taki login jest już zajęty",
+    });
+});
 
-app.get("/api/loginToAccount",(req,res)=>{
-    if(req.session.user){
-    res.send({logged:true,user:req.session.user})
-}
-    else {
-        res.send({logged:false})
+app.get("/api/loginToAccount", (req, res) => {
+  if (req.session.user) {
+    res.send({ logged: true, user: req.session.user });
+  } else {
+    res.send({ logged: false });
+  }
+});
+
+app.post("/api/loginToAccount", async (req, res) => {
+  const { loginLogin, loginPassword } = req.body;
+
+  const checkLogin = await student.findOne({
+    where: {
+      login: loginLogin,
+    },
+  });
+  if (checkLogin == null) {
+    req.session.logged = false;
+    res.send({
+      message: "Błędny login",
+    });
+  } else if (checkLogin) {
+    if (await bcrypt.compare(loginPassword, checkLogin.haslo)) {
+      req.session.user = checkLogin;
+      req.session.logged = true;
+      res.send({
+        message: "Pomyślne zalogowanie",
+      });
+    } else {
+      res.send({
+        message: "Hasło nie jest poprawne",
+      });
+      req.session.logged = false;
     }
-})
+  }
+});
 
-app.post('/api/loginToAccount', async (req, res) => {
-    
-    const {
-        loginLogin,
-        loginPassword
-    } = req.body
+app.post("/api/logoutFromAccount", (req, res) => {
+  req.session.destroy();
+  res.clearCookie("key");
+  res.end();
+});
 
-    const checkLogin = await student.findOne({
-        where: {
-            login: loginLogin
-        }
-    })
-    if (checkLogin == null){
-    req.session.logged = false
-        res.send({
-            message: 'Błędny login'
-        });
-    }
-    else if (checkLogin) {
-        if (await bcrypt.compare(loginPassword, checkLogin.haslo)){
-            req.session.user = checkLogin
-            req.session.logged = true
-            res.send({
-                message: 'Pomyślne zalogowanie'
-            });
-            
-          
-        }
-        else {
-            res.send({
-                message: 'Hasło nie jest poprawne'
-            });
-            req.session.logged = false
-        }
-    }
+app.get("/api/getStudents", async (req, res) => {
+  const listStudent = await student.findAll();
 
-})
-
-app.post('/api/logoutFromAccount',(req,res)=>{  
-    req.session.destroy()
-    res.clearCookie("key");
-    res.end()
-})
-
-
-app.get('/api/getStudents', async (req, res)=>{
-    const listStudent = await student.findAll()
-
-    res.send(listStudent)
-})
-app.post('/api/changePasswordToAccount', async (req,res)=>{
-    const {changePassword,changePassword2} = req.body
-    if(changePassword==changePassword2){
+  res.send(listStudent);
+});
+app.post("/api/changePasswordToAccount", async (req, res) => {
+  const { changePassword, changePassword2 } = req.body;
+  if (changePassword == changePassword2) {
     const hashedPassword = await bcrypt.hash(changePassword, 10);
-    await student.update({haslo: hashedPassword},
-    {where:{login:req.session.user.login}})
-    res.send({message:'Pomyślnie zmieniono hasło do konta'})
-}
-else res.send({message:'Hasła się nie zgadzają'})
-})
+    await student.update(
+      { haslo: hashedPassword },
+      { where: { login: req.session.user.login } }
+    );
+    res.send({ message: "Pomyślnie zmieniono hasło do konta" });
+  } else res.send({ message: "Hasła się nie zgadzają" });
+});
 
-app.post('/api/createForm', async (req, res) => {
-    try {
-        //await db.sequelize.transaction(async function (t) {
-            const {
-                login,
-                haslo,
-                imie,
-                nazwisko,
-                indeks,
-                studia,
-                kierunek,
-                specjalnosc,
-                rok_studiow,
-                rodzaj_studiow,
-                telefon,
-                email,
-                rola
-            } = req.body
+app.post("/api/createForm", async (req, res) => {
+  try {
+    //await db.sequelize.transaction(async function (t) {
+    const {
+      login,
+      haslo,
+      imie,
+      nazwisko,
+      indeks,
+      studia,
+      kierunek,
+      specjalnosc,
+      rok_studiow,
+      rodzaj_studiow,
+      telefon,
+      email,
+      rola,
+    } = req.body;
 
-            student.create({
-                login: login,
-                haslo: await bcrypt.hash(haslo, 10),
-                imie: imie,
-                nazwisko: nazwisko,
-                indeks: indeks,
-                studia: studia,
-                kierunek: kierunek,
-                specjalnosc: specjalnosc,
-                rok_studiow: rok_studiow,
-                rodzaj_studiow: rodzaj_studiow,
-                telefon: telefon,
-                email: email,
-                rola: rola
-            }
-            // , {
-            //     transaction: t,
-            // }
-            )
-            console.log("Wysłano")
-            res.send({
-                message: 'pomyślnie wysłano ;)'
-            //});
-        })
-    } catch {
-        console.log("Błąd")
-        res.send({
-            message: 'Błąd ;)'
-        });
-    }
+    student.create(
+      {
+        login: login,
+        haslo: await bcrypt.hash(haslo, 10),
+        imie: imie,
+        nazwisko: nazwisko,
+        indeks: indeks,
+        studia: studia,
+        kierunek: kierunek,
+        specjalnosc: specjalnosc,
+        rok_studiow: rok_studiow,
+        rodzaj_studiow: rodzaj_studiow,
+        telefon: telefon,
+        email: email,
+        rola: rola,
+      }
+      // , {
+      //     transaction: t,
+      // }
+    );
+    console.log("Wysłano");
+    res.send({
+      message: "pomyślnie wysłano ;)",
+      //});
+    });
+  } catch {
+    console.log("Błąd");
+    res.send({
+      message: "Błąd ;)",
+    });
+  }
+});
 
+//Zmiana ról w panelu administratora
 
-})
+app.put("/api/changeRole", async (req, res) => {
+  const { action, type, id } = req.body;
+  updatedStudent = await student.update({ [action]: type }, { where: { id: id } });
+  res.send(updatedStudent);
+});
